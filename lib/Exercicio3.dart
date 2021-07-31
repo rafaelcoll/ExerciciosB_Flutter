890584e1a862f1dee1be271af46da30edc2c1a40
Rafael Coll <rafaelcoll@terra.com.br>
2021-07-31 18:00:20 -0300

    Exercícios finalizados

---
+++ lib/Exercicio3.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        scaffoldBackgroundColor: Colors.lightGreen.shade100,
+      ),
+      home: MyHomePage(title: 'Flutter Third Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter1 = 0;
+  int _counter2 = 0;
+  void _incrementCounter1() {
+    setState(() {
+      _counter1++;
+    });
+  }
+  void _incrementCounter2() {
+    setState(() {
+      _counter2++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button 1 this many times:',
+            ),
+            Text(
+              '$_counter1',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              'You have pushed the button 2 this many times:',
+            ),
+            Text(
+              '$_counter2',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter1,
+            tooltip: 'Decrement',
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _incrementCounter2,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+        ]
+      )
+    );
+  }
+}
---
+++ lib/Exercicio4.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Bid Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _maxBid = 0;
+  double increment = 50;
+  void _increaseBid() {
+    setState(() {
+      _maxBid += increment;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'My Maximum Bid: $_maxBid',
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.add_circle),
+                  tooltip: 'Increase bid by 50',
+                  onPressed: _increaseBid,
+                ),
+                Text(
+                  'Increase Bid',
+                  style: Theme.of(context).textTheme.bodyText1,
+
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
